7ec37bb7a4fff4c399ac8b273ac5aecff4890fb6
bl1nkker <nurovich14@gmail.com>
2024-03-21 22:38:11 +0500

    introduced goroutines

--- second/second.go
+++ second/second.go
@@ -1,5 +1,17 @@
 package second
 
+import (
+	"fmt"
+	"time"
+)
+
 func RunSecond(){
-	
+	go PrintSomething("This is the first thing to be printed")
+	// Without sleep goroutine above will not display anything
+	time.Sleep(1 * time.Second)
+	PrintSomething("This is the second thing to be printed")
+}
+
+func PrintSomething(s string){
+	fmt.Println(s)
 }
