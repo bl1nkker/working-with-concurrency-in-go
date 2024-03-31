33172c102a593ee22b68fd2547c2a084191f1fe5
bl1nkker <nurovich14@gmail.com>
2024-03-31 18:01:04 +0500

    added makePizza function

--- producerconsumer/producerconsumer.go
+++ producerconsumer/producerconsumer.go
@@ -1,6 +1,12 @@
 package producerconsumer
 
-import "github.com/fatih/color"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
 
 const PizzasAmount = 10
 
@@ -31,11 +37,46 @@ func (p *Producer) Close() error {
     return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder{
+	pizzaNumber++
+	if pizzaNumber <= PizzasAmount{
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order number #%d\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		message := ""
+		success := false
+
+		if rnd < 5{
+			pizzasFailed ++
+		} else{
+			pizzasMade ++
+		}
+		total ++
+		fmt.Printf("Making pizza #%d. It will take %d seconds", pizzaNumber, delay)
+
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2{
+			message = fmt.Sprintf("*** We ran out of ingridients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4{
+			message = fmt.Sprintf("*** The cook quit while making the pizza #%d!", pizzaNumber)
+		} else{
+			success = true
+			message = fmt.Sprintf("Pizza order #%d is ready!", pizzaNumber)
+		}
+		return &PizzaOrder{pizzaNumber: pizzaNumber, message: message, success: success}
+	}
+	return &PizzaOrder{pizzaNumber: pizzaNumber}
+}
+
 func pizzeria(pizzaMaker *Producer){
 	// keep track of which pizza we are making
-
+	i := 0
 	// run forever, or until we receive a quit notification
-
+	for {
+		currentPizza := makePizza(i)
+	}
 	// try to make pizzas
 }
 
